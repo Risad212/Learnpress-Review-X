05b601eb37883dbc4464bc0a8bca3c8f3196e6da
Hafez Risad <83496132+Risad212@users.noreply.github.com>
2026-04-27 17:21:33 +0600

    Add LearnPress ReviewX plugin file

---
+++ learnpress-reviewx.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:       LearnPress ReviewX
+ * Plugin URI:        
+ * Description:       Simple course review and rating system for LearnPress.
+ * Version:           1.0.0
+ * Requires at least: 5.8
+ * Requires PHP:      7.4
+ * Author:            hmrisad
+ * Author URI:        
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       learnpress-reviewx
+ * Domain Path:       /languages
+ * Requires Plugins:  learnpress
+ */
