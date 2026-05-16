cf433ad883bbf992dbbf670058af0a803b37eb46
Hafez Risad <83496132+Risad212@users.noreply.github.com>
2026-05-16 19:16:42 +0600

    Refactor Course Review plugin initialization

--- course-review.php
+++ course-review.php
@@ -2,11 +2,11 @@
 
 /**
  * Plugin Name: Course Review
- * Plugin URI: 
+ * Plugin URI:
  * Description: course review addon for learnpress.
  * Author: hmrisad
  * Version: 1.0.0
- * Author URI: 
+ * Author URI:
  * Tags: learnpress
  * Requires at least: 6.0
  * Requires PHP: 7.4
@@ -16,60 +16,100 @@
  * Requires Plugins: learnpress
  */
 
+use LearnPress\CourseReview\TemplateHooks\TemplateHooks;
+use LearnPress\CourseReview\CourseReviewShortCode;
 
 if (!defined('ABSPATH')) exit;
 
-// point to root folder of plugin where anywhere
-const COURSE_REVIEW_FILE = __FILE__;
-
-require __DIR__ . '/vendor/autoload.php';
-
-
-function course_review_preload()
+final class Course_Review_Preload
 {
-    // Set Base name plugin plugin folder and main file
-    define('COURSE_REVIEW_BASENAME', plugin_basename(__FILE__));
 
-    include_once ABSPATH . 'wp-admin/includes/plugin.php';
+    /**
+	 * @var LP_Addon_Course_Review $addon
+	 */
+	public static $addon;
 
-    $addon_info = get_file_data(
-        __FILE__,
-        array(
-            'Name'               => 'Plugin Name',
-            'Require_LP_Version' => 'Require_LP_Version',
-            'Version'            => 'Version',
-        )
-    );
+	public static $instance = null;
 
-    define('COURSE_REVIEW_VER', $addon_info['Version']);
-    define('COURSE_REVIEW_REQUIRE_VER', $addon_info['Require_LP_Version']);
+	/**
+	 * Singleton instance
+	 */
+	public static function instance()
+	{
+		if (is_null(self::$instance)) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
 
-    // Check LearnPress Activated
-    if (! is_plugin_active('learnpress/learnpress.php')) {
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		add_action('plugins_loaded', [$this, 'preload']);
+	}
 
-        add_action('admin_notices', function () use ($addon_info) {
-            echo '<div class="notice notice-error"><p>';
-            echo 'Please activate <strong>LearnPress version ' . COURSE_REVIEW_REQUIRE_VER . ' or later</strong> before activating <strong>' . $addon_info['Name'] . '</strong>';
-            echo '</p></div>';
-        });
+	/**
+	 * Plugin preload check
+	 */
+	public function preload()
+	{
+		define('COURSE_REVIEW_FILE', __FILE__);
+		define('COURSE_REVIEW_BASENAME', plugin_basename(__FILE__));
 
-        deactivate_plugins(COURSE_REVIEW_BASENAME);
+		include_once ABSPATH . 'wp-admin/includes/plugin.php';
 
-        if (isset($_GET['activate'])) {
-            unset($_GET['activate']);
-        }
+		$addon_info = get_file_data(
+			__FILE__,
+			[
+				'Name'               => 'Plugin Name',
+				'Require_LP_Version' => 'Require_LP_Version',
+				'Version'            => 'Version',
+			]
+		);
 
-        return;
-    }
+		define('COURSE_REVIEW_VER', $addon_info['Version']);
+		define('COURSE_REVIEW_REQUIRE_VER', $addon_info['Require_LP_Version']);
 
-    // LearnPress loaded
-    add_action('learn-press/ready', function () {
-        include_once plugin_dir_path(__FILE__) . 'inc/plugin.php';
-        include_once plugin_dir_path(__FILE__) . 'inc/CourseReviewShortcode.php';
+		// Check LearnPress active
+		if (!is_plugin_active('learnpress/learnpress.php')) {
 
-        Course_Review_Addon::instance();
-        CourseReviewShortCode::instance();
-    });
+			add_action('admin_notices', function () use ($addon_info) {
+				echo '<div class="notice notice-error"><p>';
+				echo 'Please activate <strong>LearnPress version ' . COURSE_REVIEW_REQUIRE_VER . ' or later</strong> before activating <strong>' . $addon_info['Name'] . '</strong>';
+				echo '</p></div>';
+			});
+
+			deactivate_plugins(COURSE_REVIEW_BASENAME);
+
+			if (isset($_GET['activate'])) {
+				unset($_GET['activate']);
+			}
+
+			return;
+		}
+
+		// Load plugin after LearnPress ready
+		add_action('learn-press/ready', [$this, 'init_plugin']);
+	}
+
+	/**
+	 * Initialize addon
+	 */
+	public function init_plugin()
+	{ 
+        require __DIR__ . '/vendor/autoload.php';
+		include_once 'inc/plugin.php';
+
+		self::$addon = Course_Review_Addon::instance();
+
+		TemplateHooks::instance();
+		CourseReviewShortCode::instance();
+	}
 }
 
-add_action('plugins_loaded', 'course_review_preload');
+/**
+ * Start plugin
+ */
+Course_Review_Preload::instance();
